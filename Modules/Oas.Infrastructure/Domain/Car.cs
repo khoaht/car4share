b746e5aecfff96e628e270b547fb0b53f6d2b7ae
khoaht <hotankhoa.qn@gmail.com>
2014-05-21 23:37:24 +0700

    added Domain model

---
+++ Modules/Oas.Infrastructure/Domain/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oas.Infrastructure.Domain
+{
+    public class Car
+    {
+        public string Name { get; set; }
+
+        public Guid CategoryId { get; set; }
+
+        public Guid CarSourceId { get; set; }
+
+        public int YearOfProduct { get; set; }
+
+        public string Model { get; set; }
+
+        public CarType CarType { get; set; }
+
+        [ForeignKey("CarSourceId")]
+        public CarSource CarSource { get; set; }
+
+
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+    }
+
+    public enum CarType
+    {
+        AT,
+        MT
+    }
+}
---
+++ Modules/Oas.Infrastructure/Domain/CarSource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oas.Infrastructure.Domain
+{
+    public class CarSource
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Car> Cars { get; set; }
+    }
+
+}
